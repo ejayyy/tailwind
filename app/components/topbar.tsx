2eeb9104add3a18cfa4addcc46dde6f51c5ae3a5
ejayyy <ejay0811@gmail.com>
2023-10-14 19:52:45 -0700

    Add google material/symbol

--- app/components/topbar.tsx
+++ app/components/topbar.tsx
@@ -2,6 +2,7 @@
 
 import { Fragment, useState } from 'react'
 import Link from "next/link"
+import Close from '@material-symbols/svg-400/outlined/close.svg';
 
 export default function Topbar() {
     const [open, setOpen] = useState(true)
@@ -14,13 +15,11 @@ export default function Topbar() {
                     <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
                         <div className={(open && "translate-x-0" || "translate-x-full") + " pointer-events-auto relative w-screen max-w-md transform transition ease-in-out duration-500 sm:duration-700"}>
                             <div className={(open && "opacity-100" || "opacity-0") + " absolute right-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4 ease-in-out duration-500"}>
-                                <button type="button" className="relative rounded-md text-gray-300 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-white"
+                                <button type="button" className="relative rounded-md group focus:outline-none focus:ring-2 focus:ring-white"
                                     onClick={() => setOpen(false)}>
                                     <span className="absolute -inset-2.5"></span>
                                     <span className="sr-only">Close panel</span>
-                                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
+                                    <Close width={20} height={20} className="fill-gray-300 group-hover:fill-gray-500" />
                                 </button>
                             </div>
                             <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
